63db18d9e270b201b6472afa82fe4df4ede23c35
hafsahamadi <hafsahamadi09@gmail.com>
2025-01-31 17:33:41 +0100

    Separate footer succ

--- detail_projet.php
+++ detail_projet.php
@@ -65,57 +65,10 @@ include 'header.php'
             </div>
     </section>
 
-    <section data-bs-version="5.1" class="footer1 careerm5 cid-uAZlXtgzRP" once="footers" id="footer-1-uAZlXtgzRP">
-<div class="container-fluid">
-        <div class="row">
-            <div class="col-12 col-lg-3 col-md-6">
-                <div class="title-wrapper">
-                    <h3 class="mbr-section-title mbr-fonts-style display-5">À Propos</h3>
-                </div>
-            </div>
-            <div class="col-12 col-lg-3 col-md-6">
-                <div class="contacts-wrapper">
-                    <h4 class="mbr-section-subtitle mbr-fonts-style display-7">Suivez-Nous</h4>
-                    <ul class="list mbr-fonts-style display-4">
-                        <li class="item-wrap">                            Adresse:
-                             Morocco</li>
-                        <li class="item-wrap">Téléphone: 
-                            <a href="tel:01 23 45 67 89" class="text-white">01 23 45 67 89</a></li>
-                        <li class="item-wrap">Email:
-                            <a href="mailto:contact@pssii.fr" class="text-white">contact@pssii.fr</a> </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-12 col-lg-3 col-md-6">
-                <div class="list-wrapper">
-                    <h4 class="mbr-section-subtitle mbr-fonts-style display-7">Liens Utiles</h4>
-                    <ul class="list mbr-fonts-style display-4">
-                        <li class="item-wrap"><a href="index.php" class="text-white">Accueil</a></li>
-                        <li class="item-wrap"><a href="projets.php" class="text-white">Projets</a></li>
-                        <li class="item-wrap"><a href="publications.php" class="text-white">Publications</a></li>
-                        <li class="item-wrap"><a href="membres.php" class="text-white">Membres</a></li>
-                        <li class="item-wrap"><a href="contact.html" class="text-white">Contact</a></li>
-            
-                    </ul>
-                </div>
-            </div>
-            <div class="col-12 col-lg-3 col-md-6">
-                <div class="soc-wrapper">
-                    <h4 class="mbr-section-subtitle mbr-fonts-style display-7">Réseaux Sociaux</h4>
-                    <ul class="list mbr-fonts-style display-4">
-                        <li class="item-wrap"><a href="#" class="text-white">Facebook</a></li>
-                        <li class="item-wrap"><a href="#" class="text-white">Twitter</a></li>
-                        <li class="item-wrap"><a href="#" class="text-white">LinkedIn</a></li>
-                        <li class="item-wrap"><a href="#" class="text-white">Instagram</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-12">
-                <p class="copyright mbr-fonts-style display-4">© 2025 PSSII. Tous droits réservés.</p>
-            </div>
-        </div>
-    </div>
-</section>
+    <?php
+include 'footer.php'
+?>
+
 
      <script>
     document.addEventListener('DOMContentLoaded', function() {
